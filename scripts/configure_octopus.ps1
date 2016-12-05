509ffd508d2b942e4a3895bcbe7265705ff02ef9
Joe Beaudry <joe.beaudry@slalom.com>
2016-12-05 14:44:28 -0500

    Maded some tweaks to fix the SQL install, and added further verbosity to the Octopus Configuration

--- scripts/configure_octopus.ps1
+++ scripts/configure_octopus.ps1
@@ -1,5 +1,6 @@
 $ConfScript = @"
 
+
 function Invoke-InstallSQL{
 Write-Output "Downloading SQL"
 
@@ -27,10 +28,12 @@ start-process `$output "/q /x:C:\SQL\SqlExpr" -wait -verb RunAs
 `$time = Get-Date
 write-output "Finished Extracting SQL Download- `$Time" | Out-File -Append C:\logs.txt 
 
+`$SQL =  "C:\SQL\SqlExpr\SETUP.EXE"
 `$time = Get-Date
 write-output "Installing SQL - `$Time" | Out-File -Append C:\logs.txt 
 Write-Output "Installing SQL"
-Start-Process `$SQL '/Q /ACTION=Install /FEATURES=SQLEngine /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT="vagrant" /SQLSVCPASSWORD="vagrant" /SQLSYSADMINACCOUNTS="vagrant" /AGTSVCACCOUNT="NT AUTHORITY\System" /TCPENABLED=1 /IACCEPTSQLSERVERLICENSETERMS' -nonewwindow -wait -Verb RunAs | Out-File -Append C:\Logs.txt
+`$InstallFlags = '/Q /ACTION=Install /FEATURES=SQLEngine /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT=`"vagrant`" /SQLSVCPASSWORD=`"vagrant`" /SQLSYSADMINACCOUNTS`="vagrant" /AGTSVCACCOUNT=`"NT AUTHORITY\System`" /TCPENABLED=1 /IACCEPTSQLSERVERLICENSETERMS'
+Start-Process `$SQL -ArgumentList `$InstallFlags -wait -Verb RunAs | Out-File -Append C:\Logs.txt
 
 
 `$time = Get-Date
@@ -38,25 +41,30 @@ write-output "Finished Installing SQL - `$Time" | Out-File -Append C:\logs.txt
 
 }
 
+
 function Invoke-ConfigureOctopus{
 `$Exe = "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" 
 
 `$time = Get-Date
 write-output "Configuring OctoInstance - `$Time" | Out-File -Append C:\logs.txt 
+ 
 
 Write-Output "Configuring Instance"
-Start-Process `$Exe 'create-instance --instance="OctopusServer" --config "C:\Octopus\OctopusServer.config"' -Wait -Verb RunAs
+Start-Process `$Exe 'create-instance --instance="OctopusServer" --config "C:\Octopus\OctopusServer.config"' -NoNewWindow -Wait | Out-File -Append C:\Logs.txt
 Write-Output "Configuring DB/Base Settings"
-Start-Process `$Exe "configure --instance `"OctopusServer`" --home `"C:\Octopus`" --storageConnectionString `"Data Source=(local);Initial Catalog=Octo;Integrated Security=True;User ID=vagrant;Password=vagrant`" --upgradeCheck `"True`" --upgradeCheckWithStatistics `"True`" --webAuthenticationMode `"UsernamePassword`" --webForceSSL `"False`" --webListenPrefixes `"http://localhost:80/`" --commsListenPort `"10943`" --serverNodeName `"`$env:ComputerName`"" -Wait -Verb RunAs
-Start-Process `$Exe 'database --instance "OctopusServer" --create --grant "NT AUTHORITY\SYSTEM"' -Wait -Verb RunAs
+
+`$ArgList = 'configure --instance "OctopusServer" --home "C:\Octopus" --storageConnectionString "Data Source=(local);Initial Catalog=Octo;Integrated Security=True;User ID=vagrant;Password=vagrant" --upgradeCheck "True" --upgradeCheckWithStatistics "True" --webAuthenticationMode "UsernamePassword" --webForceSSL "False" --webListenPrefixes "http://localhost:80/" --commsListenPort "10943"' + " --serverNodeName `$env:ComputerName"
+Start-Process `$Exe -ArgumentList `$ArgList -NoNewWindow -Wait | Out-File -Append C:\Logs.txt
+
+Start-Process `$Exe 'database --instance "OctopusServer" --create --grant "NT AUTHORITY\SYSTEM"' -NoNewWindow -Wait | Out-File -Append C:\Logs.txt
 Write-Output "Creating Service"
-Start-Process `$Exe 'service --instance="OctopusServer" --stop' -Wait -Verb RunAs
+Start-Process `$Exe 'service --instance="OctopusServer" --stop' -NoNewWindow -Wait | Out-File -Append C:\Logs.txt
 Write-Output "Creating Admin"
-Start-Process `$Exe 'admin --instance "OctopusServer" --username "admin" --email "admin@admin.com" --password "Vagrant!"' -Wait -Verb RunAs
+Start-Process `$Exe 'admin --instance "OctopusServer" --username "admin" --email "admin@admin.com" --password "Vagrant!"' -NoNewWindow -Wait | Out-File -Append C:\Logs.txt
 Write-Output "Applying License"
 Start-Process `$Exe 'license --instance="OctopusServer" --licenseBase64="PExpY2Vuc2UgU2lnbmF0dXJlPSJXUzg0ZXUzUzR6MWFXRm1nY3h2NUtNUHh3RHZhMkwvVWFOTC9PRWpQdHhiWDNGMzVWVmNEMDVKeGFRaXlUY09wbU1pNVJsRGYyV3dWY2hleElHUENqdz09Ij4NCiAgPExpY2Vuc2VkVG8+VGVzdDwvTGljZW5zZWRUbz4NCiAgPExpY2Vuc2VLZXk+NDkyOTUtMDQzMzctMjg0MjMtMjI1MTk8L0xpY2Vuc2VLZXk+DQogIDxWZXJzaW9uPjIuMDwhLS0gTGljZW5zZSBTY2hlbWEgVmVyc2lvbiAtLT48L1ZlcnNpb24+DQogIDxWYWxpZEZyb20+MjAxNi0xMi0wMjwvVmFsaWRGcm9tPg0KICA8VmFsaWRUbz4yMDE3LTAxLTE2PC9WYWxpZFRvPg0KICA8UHJvamVjdExpbWl0PlVubGltaXRlZDwvUHJvamVjdExpbWl0Pg0KICA8TWFjaGluZUxpbWl0PlVubGltaXRlZDwvTWFjaGluZUxpbWl0Pg0KICA8VXNlckxpbWl0PlVubGltaXRlZDwvVXNlckxpbWl0Pg0KPC9MaWNlbnNlPg==" --wait="5000"' -Wait -Verb RunAs
 Write-Output "Restarting service"
-Start-Process `$Exe 'service --instance="OctopusServer" --install --reconfigure --start' -Wait -Verb RunAs
+Start-Process `$Exe 'service --instance="OctopusServer" --install --reconfigure --start' -NoNewWindow -Wait | Out-File -Append C:\Logs.txt
 
 `$time = Get-Date
 write-output "Finished Configuring Octo Instance - `$Time" | Out-File -Append C:\logs.txt 
